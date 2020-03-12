348384bb95c1ce4f515614f7f0716350a01de345
Gurkalov Dmitry <gurkalov.dev@gmail.com>
2020-03-12 09:20:30 +0300

    Add ge and le filters for users list

--- src/app/Http/Controllers/Traits/CrudTrait.php
+++ src/app/Http/Controllers/Traits/CrudTrait.php
@@ -80,6 +80,14 @@ trait CrudTrait
         foreach ($filter as $field => $value) {
             $query->where($field, '=', $value);
         }
+        $filter = $request->get('filterge', []);
+        foreach ($filter as $field => $value) {
+            $query->where($field, '>=', $value);
+        }
+        $filter = $request->get('filterle', []);
+        foreach ($filter as $field => $value) {
+            $query->where($field, '<=', $value);
+        }
     }
 
     /**
